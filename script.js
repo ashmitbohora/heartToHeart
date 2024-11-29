7f1b782c8dce23864babe758d3344a5c5a421469
gamerszzz <gamerashmit@gmail.com>
2024-11-28 21:05:39 -0600

    Lots of changes made

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-
-
 const handClicker = document.querySelector(".fa-hand-pointer");
 const envelopeCard = document.querySelector(".envelopeCard");
 
-
-handClicker.addEventListener("click", () => {
-
-
-    envelopeCard.classList.toggle("active");
-
-
-
-
-});
+if (handClicker && envelopeCard) {
+    handClicker.addEventListener("click", () => {
+        envelopeCard.classList.toggle("active");
+    });
+}
 
 
 const menu = document.querySelector(".fa-bars");
 const xMark = document.querySelector(".fa-xmark");
 const navMobile = document.querySelector(".navMobile");
+const links = document.querySelectorAll(".navMobile .more a");
 
+if (menu && xMark && navMobile) {
+    menu.addEventListener("click", () => {
+        navMobile.classList.toggle("hidden");
+    });
 
+    xMark.addEventListener("click", () => {
+        navMobile.classList.add("hidden");
+    });
 
-menu.addEventListener("click", () => {
-    navMobile.classList.toggle("hidden");
-});
-
-xMark.addEventListener("click", () => {
-    navMobile.classList.toggle("hidden");
-});
+    links.forEach(link => {
+        link.addEventListener("click", () => {
+            navMobile.classList.add("hidden");
+        });
+    });
+}
