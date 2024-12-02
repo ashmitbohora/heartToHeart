8277dc2d1db192a9e14fa19d10f4e71632bd17d5
gamerszzz <gamerashmit@gmail.com>
2024-12-02 13:05:53 -0600

    added slider and fixed minor changes

--- script.js
+++ script.js
@@ -28,3 +28,42 @@ if (menu && xMark && navMobile) {
         });
     });
 }
+
+
+
+
+
+
+const leftArrow = document.querySelector(".fa-arrow-left");
+const rightArrow = document.querySelector(".fa-arrow-right");
+const imageSlider = document.querySelector(".sliderContainer");
+let imageIndex = 0;
+const images = document.querySelectorAll(".sliderContainerImage");
+
+function sliderUpdate(){
+    const imageWidth = images[0].clientWidth;
+    imageSlider.scrollTo({
+        left: imageIndex * imageWidth,
+        behavior: "smooth",
+    });
+}
+
+leftArrow.addEventListener("click", () => {
+
+    if (imageIndex > 0){
+        imageIndex--;
+    } else {
+        imageIndex = images.length - 1;
+    }
+    sliderUpdate()
+});
+
+rightArrow.addEventListener("click", () => {
+
+    if (imageIndex < images.length - 1) {
+        imageIndex++;
+    } else {
+        imageIndex = 0; 
+    }
+    sliderUpdate();
+});
